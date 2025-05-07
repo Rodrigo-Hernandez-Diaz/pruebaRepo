d9518a8dca083ad0ceae1b616e87c89b22f775db
rodri <rherdia609@g.educaand.es>
2025-05-07 20:01:12 +0200

    adios caracol

--- src/Parte1/CCuenta.java
+++ src/Parte1/CCuenta.java
@@ -8,42 +8,46 @@ package Parte1;
  *
  * @author Alumno Tarde
  */
-
 public class CCuenta {
+
     protected double dSaldo;
     protected int cont = 0;
-    
+
     public CCuenta(double dSaldo) {
         this.dSaldo = dSaldo;
     }
-   
-    public int ingresar(double cantidad) {
-    int iCodErr;
-    if (cantidad < 0) {
-        System.out.println("No se puede ingresar una cantidad negativa");
-        iCodErr = 1;
-    } else if (cantidad == -3) {
-        System.out.println("Error detectable en pruebas de caja blanca");
-        iCodErr = 2;
-    } else {
-        // Depuración. Punto de parada. Solo en el 3 ingreso
-        dSaldo = dSaldo + cantidad;
-        cont++;
-        iCodErr = 0;
-    }
-    // Depuración. Punto de parada cuando la cantidad es menor de 0
-    return iCodErr;
-}
-   
-    public void retirar(double cantidad) {
-    if (cantidad <= 0) {
-        System.out.println("No se puede retirar una cantidad negativa");
-    } else if (dSaldo < cantidad) {
-        System.out.println("No hay suficiente saldo");
-    } else {
-        dSaldo -= cantidad;
-        System.out.println("Saldo Restante: " + dSaldo);
-    }
-}
-}
 
+    public int ingresar(double cantidad) {
+        int iCodErr;
+        if (cantidad < 0) {
+            System.out.println("No se puede ingresar una cantidad negativa");
+            iCodErr = 1;
+        } else if (cantidad == -3) {
+            System.out.println("Error detectable en pruebas de caja blanca");
+            iCodErr = 2;
+        } else {
+            // Depuración. Punto de parada. Solo en el 3 ingreso
+            dSaldo = dSaldo + cantidad;
+            cont++;
+            iCodErr = 0;
+        }
+        // Depuración. Punto de parada cuando la cantidad es menor de 0
+        return iCodErr;
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("No se puede retirar una cantidad negativa");
+        } else if (dSaldo < cantidad) {
+            System.out.println("No hay suficiente saldo");
+        } else {
+            dSaldo -= cantidad;
+            System.out.println("Saldo Restante: " + dSaldo);
+        }
+
+    }
+
+    public void lblblb() {
+        System.out.println("blablabla blebleble blublublu");
+    }
+}
